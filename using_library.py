5c36b1f12f957b25a9d0303ecfeb2d58aa274d81
krrish <krrishniresh@gmail.com>
2023-04-04 06:18:17 +0000

    final commit. hopefully!

--- using_library.py
+++ using_library.py
@@ -2,6 +2,9 @@ import holidays
 from datetime import date, datetime, timedelta
 from collections import OrderedDict
 import argparse
+import pandas as pd
+
+country_codes = ['Albania: AL', 'American Samoa: AS', 'Andorra: AD', 'Angola: AO', 'Argentina: AR', 'Armenia: AM', 'Aruba: AW', 'Australia: AU', 'Austria: AT', 'Azerbaijan: AZ', 'Bahrain: BH', 'Bangladesh: BD', 'Belarus: BY', 'Belgium: BE', 'Bolivia: BO', 'Bosnia and Herzegovina: BA', 'Botswana: BW', 'Brazil: BR', 'Bulgaria: BG', 'Burundi: BI', 'Canada: CA', 'Chile: CL', 'China: CN', 'Colombia: CO', 'Costa Rica: CR', 'Croatia: HR', 'Cuba: CU', 'Curacao: CW', 'Cyprus: CY', 'Czechia: CZ', 'Denmark: DK', 'Djibouti: DJ', 'Dominican Republic: DO', 'Egypt: EG', 'Estonia: EE', 'Eswatini: SZ', 'Ethiopia: ET', 'Finland: FI', 'France: FR', 'Georgia: GE', 'Germany: DE', 'Greece: GR', 'Guam: GU', 'Honduras: HN', 'Hong Kong: HK', 'Hungary: HU', 'Iceland: IS', 'India: IN', 'Indonesia: ID', 'Ireland: IE', 'Isle of Man: IM', 'Israel: IL', 'Italy: IT', 'Jamaica: JM', 'Japan: JP', 'Kazakhstan: KZ', 'Kenya: KE', 'Kyrgyzstan: KG', 'Latvia: LV', 'Lesotho: LS', 'Liechtenstein: LI', 'Lithuania: LT', 'Luxembourg: LU', 'Madagascar: MG', 'Malawi: MW', 'Malaysia: MY', 'Malta: MT', 'Marshall Islands (the): MH', 'Mexico: MX', 'Moldova: MD', 'Monaco: MC', 'Montenegro: ME', 'Morocco: MA', 'Mozambique: MZ', 'Namibia: nan', 'Netherlands: NL', 'New Zealand: NZ', 'Nicaragua: NI', 'Nigeria: NG', 'Northern Mariana Islands (the): MP', 'North Macedonia: MK', 'Norway: NO', 'Pakistan: PK', 'Panama: PA', 'Paraguay: PY', 'Peru: PE', 'Philippines: PH', 'Poland: PL', 'Portugal: PT', 'Puerto Rico: PR', 'Romania: RO', 'Russia: RU', 'San Marino: SM', 'Saudi Arabia: SA', 'Serbia: RS', 'Singapore: SG', 'Slovakia: SK', 'Slovenia: SI', 'South Africa: ZA', 'South Korea: KR', 'Spain: ES', 'Sweden: SE', 'Switzerland: CH', 'Taiwan: TW', 'Thailand: TH', 'Tunisia: TN', 'Turkey: TR', 'Ukraine: UA', 'United Arab Emirates: AE', 'United Kingdom: GB', 'United States Minor Outlying Islands: UM', 'United States of America (the): US', 'United States Virgin Islands (the): nan', 'Uruguay: UY', 'Uzbekistan: UZ', 'Vatican City: VA', 'Venezuela: VE', 'Vietnam: VN', 'Virgin Islands (U.S.): VI', 'Zambia: ZM', 'Zimbabwe: ZW']
 
 
 # Function to check the input date format
@@ -58,7 +61,7 @@ def parse_opt():
     parser.add_argument('--sat', default=False, action='store_true', help='Include saturday as a working day')
     parser.add_argument('--country_code', type=str, default="TH", help=f'Country codes in '
                                                                        f'https://pypi.org/project/holidays/ '
-                                                                       f'(default: Thailand:TH)')
+                                                                       f'(default: Thailand:TH) {country_codes}')
     opt = parser.parse_args()
     return opt
 
